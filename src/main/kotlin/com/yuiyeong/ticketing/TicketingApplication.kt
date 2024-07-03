3d6705559ed81924455600d09f6872bb5a24cb2f
yuiyeong <joyuiyeong@gmail.com>
2024-07-04 05:54:37 +0900

    Refactor: 전체 코드 포매팅

--- src/main/kotlin/com/yuiyeong/ticketing/TicketingApplication.kt
+++ src/main/kotlin/com/yuiyeong/ticketing/TicketingApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class TicketingApplication
 
 fun main(args: Array<String>) {
-	runApplication<TicketingApplication>(*args)
+    runApplication<TicketingApplication>(*args)
 }
