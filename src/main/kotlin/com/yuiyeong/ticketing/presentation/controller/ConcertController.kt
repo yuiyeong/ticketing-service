8724a3b7e4de2fa633fb722ee4963521f2c6e9eb
yuiyeong <joyuiyeong@gmail.com>
2024-07-04 23:56:37 +0900

    Feat: ConcertController::getAvailableDates 에 대한 mock 구현

---
+++ src/main/kotlin/com/yuiyeong/ticketing/presentation/controller/ConcertController.kt
@@ -0,0 +1,35 @@
+package com.yuiyeong.ticketing.presentation.controller
+
+import com.yuiyeong.ticketing.domain.exception.InvalidTokenException
+import com.yuiyeong.ticketing.domain.exception.NotFoundConcertException
+import com.yuiyeong.ticketing.domain.exception.NotFoundTokenException
+import com.yuiyeong.ticketing.presentation.dto.ConcertAvailableDateDto
+import com.yuiyeong.ticketing.presentation.dto.response.TicketingListResponse
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/concerts")
+class ConcertController {
+    @GetMapping("{concertId}/available-dates")
+    fun getAvailableDates(
+        @RequestHeader(name = "User-Token", required = false) userToken: String?,
+        @PathVariable("concertId") concertId: Long,
+    ): TicketingListResponse<ConcertAvailableDateDto> {
+        when (userToken) {
+            null -> throw InvalidTokenException()
+            "invalidQueueToken" -> throw InvalidTokenException()
+            "notInQueueToken" -> throw NotFoundTokenException()
+        }
+        return TicketingListResponse(
+            when (concertId) {
+                1L -> listOf(ConcertAvailableDateDto(1, "2024-07-07"))
+                2L -> listOf()
+                else -> throw NotFoundConcertException()
+            },
+        )
+    }
+}
