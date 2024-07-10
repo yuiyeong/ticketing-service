40ae1efd58441796897d0fba4ca6a30e79003ae6
yuiyeong <joyuiyeong@gmail.com>
2024-07-10 22:50:07 +0900

    Feat: 결제 내역 목록 api 의 Swagger 작성

--- src/main/kotlin/com/yuiyeong/ticketing/presentation/controller/PaymentController.kt
+++ src/main/kotlin/com/yuiyeong/ticketing/presentation/controller/PaymentController.kt
@@ -1,7 +1,9 @@
 package com.yuiyeong.ticketing.presentation.controller
 
+import com.yuiyeong.ticketing.config.swagger.annotation.api.PaymentHistoryApiDoc
 import com.yuiyeong.ticketing.presentation.dto.PaymentDto
 import com.yuiyeong.ticketing.presentation.dto.response.TicketingListResponse
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,8 +11,10 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/users")
+@Tag(name = "결제", description = "결제 관련 api")
 class PaymentController {
     @GetMapping("{userId}/payments")
+    @PaymentHistoryApiDoc
     fun getPayments(
         @PathVariable("userId") userId: Long,
     ): TicketingListResponse<PaymentDto> =
