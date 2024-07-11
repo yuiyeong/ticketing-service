abfe1d0a6ddc48e28c6d4e0b8c09e77cc0c52d57
yuiyeong <joyuiyeong@gmail.com>
2024-07-11 22:27:27 +0900

    Feat: WalletController 에 WalletService 적용

--- src/main/kotlin/com/yuiyeong/ticketing/presentation/dto/WalletDto.kt
+++ src/main/kotlin/com/yuiyeong/ticketing/presentation/dto/WalletDto.kt
@@ -1,5 +1,12 @@
 package com.yuiyeong.ticketing.presentation.dto
 
+import com.yuiyeong.ticketing.application.dto.UserWalletDto
+import java.math.BigDecimal
+
 data class WalletDto(
-    val balance: Int,
-)
+    val balance: BigDecimal,
+) {
+    companion object {
+        fun from(userWalletDto: UserWalletDto): WalletDto = WalletDto(userWalletDto.balance)
+    }
+}
