8f108f0a3640cf8931f1e0b87a7e23427e209517
yuiyeong <joyuiyeong@gmail.com>
2024-07-12 03:00:33 +0900

    Feat: ConcertController 에 AvailableEventsUseCase 적용

--- src/main/kotlin/com/yuiyeong/ticketing/presentation/controller/ConcertController.kt
+++ src/main/kotlin/com/yuiyeong/ticketing/presentation/controller/ConcertController.kt
@@ -1,12 +1,11 @@
 package com.yuiyeong.ticketing.presentation.controller
 
+import com.yuiyeong.ticketing.application.usecase.AvailableEventsUseCase
 import com.yuiyeong.ticketing.config.swagger.annotation.api.AvailableConcertEventsApiDoc
-import com.yuiyeong.ticketing.domain.exception.InvalidTokenException
-import com.yuiyeong.ticketing.domain.exception.NotFoundConcertException
-import com.yuiyeong.ticketing.domain.exception.NotFoundTokenException
 import com.yuiyeong.ticketing.presentation.dto.ConcertAvailableDateDto
 import com.yuiyeong.ticketing.presentation.dto.response.TicketingListResponse
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestHeader
@@ -17,23 +16,19 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/v1/concerts")
 @Tag(name = "콘서트", description = "콘서트 관련 api")
 class ConcertController {
+    @Autowired
+    private lateinit var availableEventsUseCase: AvailableEventsUseCase
+
     @GetMapping("{concertId}/available-events")
     @AvailableConcertEventsApiDoc
     fun getAvailableEvents(
         @RequestHeader(name = "User-Token", required = false) userToken: String?,
         @PathVariable("concertId") concertId: Long,
     ): TicketingListResponse<ConcertAvailableDateDto> {
-        when (userToken) {
-            null -> throw InvalidTokenException()
-            "invalidQueueToken" -> throw InvalidTokenException()
-            "notInQueueToken" -> throw NotFoundTokenException()
-        }
-        return TicketingListResponse(
-            when (concertId) {
-                1L -> listOf(ConcertAvailableDateDto(1, "2024-07-07"))
-                2L -> listOf()
-                else -> throw NotFoundConcertException()
-            },
-        )
+        val list =
+            availableEventsUseCase
+                .getConcertEvents(concertId)
+                .map { ConcertAvailableDateDto.from(it) }
+        return TicketingListResponse(list)
     }
 }
