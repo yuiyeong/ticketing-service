bd54c5a1d9777267e1ec2f46aa9b1935e4982b8f
yuiyeong <joyuiyeong@gmail.com>
2024-07-19 20:29:08 +0900

    Feat: UserTokenInterceptor 와 관련 Config 및 annotation 추가

---
+++ src/main/kotlin/com/yuiyeong/ticketing/config/WebMvcConfig.kt
@@ -0,0 +1,19 @@
+package com.yuiyeong.ticketing.config
+
+import com.yuiyeong.ticketing.application.usecase.token.ValidateTokenUseCase
+import com.yuiyeong.ticketing.presentation.interceptor.UserTokenInterceptor
+import com.yuiyeong.ticketing.presentation.resolver.EntryArgumentResolver
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfig(
+    private val validateTokenUseCase: ValidateTokenUseCase,
+    private val entryArgumentResolver: EntryArgumentResolver,
+) : WebMvcConfigurer {
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(UserTokenInterceptor(validateTokenUseCase))
+    }
+}
