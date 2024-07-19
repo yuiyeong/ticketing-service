63b8f303c88a4167b5d495d3e87d3ed73998567e
yuiyeong <joyuiyeong@gmail.com>
2024-07-19 20:00:42 +0900

    Feat: Timezone 설정

--- src/main/kotlin/com/yuiyeong/ticketing/TicketingApplication.kt
+++ src/main/kotlin/com/yuiyeong/ticketing/TicketingApplication.kt
@@ -2,10 +2,17 @@ package com.yuiyeong.ticketing
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.scheduling.annotation.EnableScheduling
+import java.util.TimeZone
 
+@EnableScheduling
+@EnableJpaAuditing
 @SpringBootApplication
 class TicketingApplication
 
 fun main(args: Array<String>) {
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+    System.setProperty("user.timezone", "UTC")
     runApplication<TicketingApplication>(*args)
 }
