be92cb710e424d50f04492fff02cf83d1ee826ef
yuiyeong <joyuiyeong@gmail.com>
2024-07-19 20:34:24 +0900

    Feat: TicketingLoggingFilter 및 관련 config 와 property 객체 추가

---
+++ src/main/kotlin/com/yuiyeong/ticketing/config/FilterConfig.kt
@@ -0,0 +1,22 @@
+package com.yuiyeong.ticketing.config
+
+import com.yuiyeong.ticketing.presentation.filter.TicketingLoggingFilter
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class FilterConfig(
+    private val loggingProperties: LoggingProperties,
+) {
+    @Bean
+    @ConditionalOnBean(TicketingLoggingFilter::class)
+    fun loggingFilter(ticketingLoggingFilter: TicketingLoggingFilter): FilterRegistrationBean<TicketingLoggingFilter> {
+        val registrationBean = FilterRegistrationBean<TicketingLoggingFilter>()
+        registrationBean.filter = ticketingLoggingFilter
+        registrationBean.addUrlPatterns("/*")
+        registrationBean.order = 1
+        return registrationBean
+    }
+}
