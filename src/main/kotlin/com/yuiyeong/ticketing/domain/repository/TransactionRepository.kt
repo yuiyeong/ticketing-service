61cb16cc3d559dd2614c1bfb664a592cb650f913
yuiyeong <joyuiyeong@gmail.com>
2024-07-19 20:09:10 +0900

    Feat: Transaction 도메인 모델과 관련 Repository 추가

---
+++ src/main/kotlin/com/yuiyeong/ticketing/domain/repository/TransactionRepository.kt
@@ -0,0 +1,11 @@
+package com.yuiyeong.ticketing.domain.repository
+
+import com.yuiyeong.ticketing.domain.model.Transaction
+
+interface TransactionRepository {
+    fun save(transaction: Transaction): Transaction
+
+    fun findOneById(id: Long): Transaction?
+
+    fun deleteAll()
+}
