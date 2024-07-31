b0bc26afe99c1f677c8c2f8fbd705d9868636897
yuiyeong <joyuiyeong@gmail.com>
2024-07-31 20:43:52 +0900

    Refactor: ExpireWaitingEntryUseCase 제거

--- src/main/kotlin/com/yuiyeong/ticketing/application/usecase/queue/ExpireWaitingEntryUseCase.kt
+++
@@ -1,7 +0,0 @@
-package com.yuiyeong.ticketing.application.usecase.queue
-
-import com.yuiyeong.ticketing.application.dto.queue.QueueEntryResult
-
-interface ExpireWaitingEntryUseCase {
-    fun execute(): List<QueueEntryResult>
-}
