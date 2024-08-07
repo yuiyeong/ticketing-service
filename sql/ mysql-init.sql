76aac676e8199abcf30b4afcfcf7ab3a82ad2333
yuiyeong <joyuiyeong@gmail.com>
2024-08-07 15:39:27 +0900

    Fix: docker compose 의 설정 수정

--- sql/ mysql-init.sql
+++ sql/ mysql-init.sql
@@ -2,6 +2,6 @@
 DROP USER IF EXISTS 'exporter'@'%';
 
 -- exporter 사용자 생성 및 권한 부여
-CREATE USER 'exporter'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+CREATE USER 'exporter'@'%' IDENTIFIED BY 'exporterpassword';
 GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'exporter'@'%';
 FLUSH PRIVILEGES;
