0416cc923eaf8ea7f980214f1572f3781beb60d8
yuiyeong <joyuiyeong@gmail.com>
2024-08-09 19:53:05 +0900

    Feat: PaymentEvent 를 Background 에서 처리하도록 수정

--- src/main/kotlin/com/yuiyeong/ticketing/application/event/PaymentEventHandler.kt
+++ src/main/kotlin/com/yuiyeong/ticketing/application/event/PaymentEventHandler.kt
@@ -2,6 +2,7 @@ package com.yuiyeong.ticketing.application.event
 
 import com.yuiyeong.ticketing.domain.event.payment.PaymentEvent
 import com.yuiyeong.ticketing.domain.notification.PaymentNotificationService
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionPhase
 import org.springframework.transaction.event.TransactionalEventListener
@@ -13,6 +14,7 @@ class PaymentEventHandler(
     /**
      * PaymentEvent 를 받아서 알림을 보내는 함수
      */
+    @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     fun handle(event: PaymentEvent) {
         paymentNotificationService.notifyPaymentResult(event)
